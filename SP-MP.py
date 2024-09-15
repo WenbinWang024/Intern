08c46314659fec438891e2a3274a46796b8aadff
WenbinWang <147360189+WenbinWang024@users.noreply.github.com>
2024-09-15 20:20:27 +0800

    Delete SP-MP.py

--- SP-MP.py
+++
@@ -1,9 +0,0 @@
-# pseudocode for saddle point mirror prox
-# input: initial point z0, learning rate eta, number of iterations T
-# output: final point zT
-
-
-# z0 initial point
-# for t = 1, 2, ..., T do
-#     w_{t+1} = argmin_{z \in Z & D} (eta * <(∇_x φ(x_t, y_t), -∇_y φ(x_t, y_t)), z>) + + D_Φ(z, z_t))
-#     z_{t+1} = argmin_{z \in Z & D} (eta * <(∇_x φ(u_{t+1}, v_{t+1}), -∇_y φ(u_{t+1}, v_{t+1})), z>) + D_Φ(z, z_t)
